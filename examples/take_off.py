a1a0eebb4cc960c43b7510b22561064a74e415b7
carmenales <carmenales@users.noreply.github.com>
2022-08-07 11:35:29 +0200

    Added comments

--- examples/take_off.py
+++ examples/take_off.py
@@ -1,12 +1,16 @@
 from djitellopy import Tello
 
+# Connect
 tello = Tello()
-
 tello.connect()
+
+# Take off
 tello.takeoff()
 
+# Move
 tello.move_left(20)
 tello.rotate_counter_clockwise(90)
 tello.move_forward(20)
 
+# Land
 tello.land()
